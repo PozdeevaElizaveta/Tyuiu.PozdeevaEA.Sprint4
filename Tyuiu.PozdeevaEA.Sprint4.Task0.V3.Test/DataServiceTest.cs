6c8251e56f2f24c8e5959c680b3ac63c728738ba
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-08 02:10:09 +0500

    Add project files.

---
+++ Tyuiu.PozdeevaEA.Sprint4.Task0.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PozdeevaEA.Sprint4.Task0.V3.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint4.Task0.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidMult()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 8, 9, 4, 1, 2, 3, 4, 5, 6, 7 };
+            int res = ds.GetMultOddArrEl(array);
+            int wait = 945;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
