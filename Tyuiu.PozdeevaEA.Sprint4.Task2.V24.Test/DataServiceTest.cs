b739a1d48a8fb76ed12d35b8f9fd5740ac6178f1
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-11-09 16:07:02 +0500

    Task2

---
+++ Tyuiu.PozdeevaEA.Sprint4.Task2.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PozdeevaEA.Sprint4.Task2.V24.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint4.Task2.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 4, 5, 8, 7, 4, 5, 7, 6, 4, 8, 7, 9 };
+            int res = ds.Calculate(array);
+            int wait = 40;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.PozdeevaEA.Sprint4.Task2.V24/Program.cs
+++ Tyuiu.PozdeevaEA.Sprint4.Task2.V24/Program.cs
@@ -1,8 +1,9 @@
-﻿using Tyuiu.PozdeevaEA.Sprint4.Task1.V24.Lib;
+﻿using Tyuiu.PozdeevaEA.Sprint4.Task2.V24.Lib;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Random rnd = new Random();
         DataService ds = new DataService();
 
         Console.Title = "Спринт #4 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
@@ -10,42 +11,35 @@ internal class Program
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #4                                                               *");
         Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
-        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Задание #2                                                              *");
         Console.WriteLine("* Вариант #24                                                             *");
         Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Дан одномерный целочисленный массив на 16 элементов заполненный         *");
-        Console.WriteLine("* значениями с клавиатуры в диапазоне от 2 до 5 подсчитать произведение   *");
-        Console.WriteLine("* нечетных элементов массива.                                             *");
-        Console.WriteLine("* С клавиатуры: 2, 2, 2, 5, 5, 3, 4, 2, 5, 2, 5, 3, 4, 3, 5, 2            *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
+        Console.WriteLine("* случайными в диапазоне от 4 до 9 подсчитать сумму нечетных элементов    *");
+        Console.WriteLine("* массива.                                                                *");
+
 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        int[] array = new int[16];
-        Console.WriteLine("Введите 16 целых чисел в диапазоне от 2 до 5:");
+        int[] array = new int[12];
+   
 
         for (int i = 0; i < array.Length; i++)
         {
-            Console.Write($"Элемент {i}: ");
-            int value = int.Parse(Console.ReadLine());
-
-            if (value < 2 || value > 5)
-            {
-                Console.WriteLine("Ошибка! Число должно быть в диапазоне от 2 до 5");
-                i--;
-                continue;
-            }
+            
+            int value = rnd.Next(4,9);
 
             array[i] = value;
         }
         Console.WriteLine();
         Console.WriteLine("Массив: ");
-        for (int i = 0; i <= 16-1; i++)
+        for (int i = 0; i <= 12 - 1; i++)
         {
-            Console.Write(array[i]);
+            Console.Write(array[i] + "\t");
         }
         Console.WriteLine();
         Console.WriteLine();
@@ -55,9 +49,8 @@ internal class Program
         Console.WriteLine("***************************************************************************");
 
         int res = ds.Calculate(array);
-        Console.WriteLine("Произведение нечетных элементов массива " + res);
+        Console.WriteLine("Сумма нечетных элементов массива " + res);
 
         Console.ReadKey();
-        
     }
 }
